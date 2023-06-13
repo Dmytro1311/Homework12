c3ae2b2bbde146d831b78e0904ce16febe4b1104
Your Name <you@example.com>
2023-06-13 11:47:23 +0300

    homework 29

--- SOAP-CLIENT/src/main/java/net/dmytro/homework27/generated/Order.java
+++ SOAP-CLIENT/src/main/java/net/dmytro/homework27/generated/Order.java
@@ -1,10 +1,10 @@
 
 package net.dmytro.homework27.generated;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * <p>Java class for order complex type.
